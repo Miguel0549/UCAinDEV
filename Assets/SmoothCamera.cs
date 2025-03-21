9292a9078c0ac05a9a6864f1fad5230384b4cf49
Pablo Beato Martínez <39810564+Private04@users.noreply.github.com>
2025-03-21 14:09:53 +0100

    Cámara v2

---
+++ Assets/SmoothCamera.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private float smoothTime = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+    private float xFollowRange = 8.0f;
+    private float yFollowRange = 1.5f;
+    [SerializeField] private Transform target;
+    void Update()
+    {
+        Vector3 targetposition = transform.position;
+        if (Math.Abs(transform.position.x-target.position.x)>xFollowRange)
+        {
+            targetposition.x = target.position.x;
+        }
+        if (Math.Abs(transform.position.y-target.position.y)>yFollowRange)
+        {
+            targetposition.y = target.position.y;
+        }
+        transform.position = Vector3.SmoothDamp(transform.position,targetposition,ref velocity,smoothTime);
+    }
+}
