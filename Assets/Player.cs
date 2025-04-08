39d8ee5317add328ce244b714adef1d2a97bd578
Pablo Beato Martínez <39810564+Private04@users.noreply.github.com>
2025-04-08 17:23:56 +0200

    Script de TestDodge/Roll

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using NUnit.Framework.Interfaces;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,6 +7,11 @@ public class Player : MonoBehaviour
     public float Speed=8.0f;
     public float WalkSpeedMultiplier=0.5f;
     public Rigidbody2D rbody2d;
+    private bool Dodging= false;
+    public float DodgeCooldown = 1.5f;
+    private float LastDodge = 0f;
+    public float DodgeDuration = 0.3f;
+    public float DodgeSpeed = 12.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +28,22 @@ public class Player : MonoBehaviour
     {
         float xMovement = Input.GetAxis("Horizontal");
         float yMovement = Input.GetAxis("Vertical");
-        rbody2d.linearVelocity = new Vector2(Speed * xMovement,Speed * yMovement);
+        if (!Dodging)
+        { 
+            rbody2d.linearVelocity = new Vector2(Speed * xMovement,Speed * yMovement);
+        }
+            if (Dodging&&Time.fixedTime-LastDodge>DodgeDuration)
+            {
+                Dodging = false;
+                rbody2d.linearVelocity = Vector2.zero;
+            }
+            if (Input.GetKey(KeyCode.Space)&&Time.fixedTime-LastDodge>DodgeCooldown)
+            {
+                Dodging=true;
+                LastDodge=Time.fixedTime;
+                rbody2d.linearVelocity = new Vector2(DodgeSpeed * xMovement,DodgeSpeed * yMovement);
+                Debug.Log("ROlling");
+            }
+        
     }
 }
